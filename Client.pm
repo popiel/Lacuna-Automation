44a6e59aad720b51974cea2391b464e115d173ae
T. Alexander Popiel <popiel@wolfskeep.com>
2010-12-26 12:53:03 -0800

    Initial checkin for public consumption

---
+++ Client.pm
@@ -0,0 +1,293 @@
+package Client;
+
+use strict;
+
+use Carp;
+use File::Path;
+use File::Spec;
+use JSON::XS;
+use LWP::UserAgent;
+use Scalar::Util qw(blessed);
+use Time::Local;
+
+sub new {
+  my $base = shift;
+  die "Cannot make a new Client from ".ref($base)."\n"
+    if blessed($base) && !$base->isa("Client");
+  my $class = ref($base) || $base;
+  my $self = ref($base) ? { %$base, @_ } : { @_ };
+  bless($self, $class);
+  $self->read_config();
+  $self->{ua} ||= LWP::UserAgent->new();
+  return $self;
+}
+
+sub read_json {
+  my $self = shift;
+  my $filename = shift;
+  my $filetype = shift;
+
+  my $file;
+# warn "filename: $filename\n";
+  unless (open($file, "<", $filename)) {
+    croak "Could not read $filetype file $filename: $!" if $filetype;
+    return;
+  }
+  my $json = join('', (<$file>));
+  close($file);
+  my $result = decode_json($json);
+  return $result;
+}
+
+sub write_json {
+  my $self = shift;
+  my $filename = shift;
+  my $filetype = shift;
+  my $value = shift;
+
+  my $dir = File::Spec->catpath((File::Spec->splitpath($filename))[0..1]);
+  -d $dir or mkpath($dir) or croak "Could not make path $dir: $!";
+
+  my $file;
+  open($file, ">", "$filename.$$") or croak "Could not write $filetype file $filename.$$: $!";
+  print $file encode_json($value);
+  close $file;
+  rename("$filename.$$", $filename) or croak "Could not rename $filetype file $filename.$$ to $filename: $!";
+}
+
+sub read_config {
+  my $self = shift;
+  croak "config not specified for Client" unless $self->{config};
+  my $config = $self->read_json($self->{config}, "config");
+  for my $key (qw(empire_name empire_password uri api_key)) {
+    $self->{$key} = $config->{$key} if exists($config->{$key});
+#    warn "$key: $self->{$key}\n";
+  }
+}
+
+sub parse_time {
+  my $str = shift;
+  return timegm($6,$5,$4,$1,$2 - 1,$3) if $str =~ /^(\d+) (\d+) (20\d\d) (\d+):(\d+):(\d+) \+0000$/;
+  return;
+}
+
+sub format_time {
+  my $time = shift;
+
+  my @elems = reverse((localtime($time))[0..5]);
+  $elems[0] += 1900;
+  $elems[1]++;
+  sprintf("%4d-%02d-%02d %02d:%02d:%02d", @elems);
+}
+
+sub call {
+  my $self = shift;
+  my $api = shift;
+  my $method = shift;
+  my @params = @_;
+
+  unshift(@params, $self->session_id) unless "$api/$method" eq "empire/login";
+
+  $api = "/$api" unless $api =~ /^\//;
+  my $message = { jsonrpc => "2.0", id => 1, method => $method, params => [ @params ] };
+  # warn "Posting to ".($self->{uri} . $api)."\n";
+  # warn "Content: ".encode_json($message)."\n";
+  my $response = $self->{ua}->post($self->{uri} . $api, Content => encode_json($message));
+  my $result = decode_json($response->content);
+  croak join(": ", $result->{error}{code}, $result->{error}{message},
+             JSON::XS->new->allow_nonref->canonical->pretty->encode($result->{error}{data}))
+    if $result->{error};
+  croak "Call failed: ".($response->status_line) unless $response->is_success;
+  croak "Call response without result" unless $result->{result};
+  $self->{session_id} = $result->{result}{session_id} if $result->{result}{session_id};
+  $self->{session_time} = time();
+  $self->write_session if $self->{session_id};
+  my $time = parse_time($result->{result}{status}{server}{time});
+  $result->{result}{status}{_time} = $time;
+  my $empire = $result->{result}{status}{empire};
+  if ($empire) {
+    $empire->{_time} = $time;
+    $self->write_json("cache/empire/$self->{empire_name}/status", empire_status => $empire);
+  }
+  my $body = $result->{result}{status}{body};
+  if ($body) {
+    $body->{_time} = $time;
+    $self->write_json("cache/body/$body->{id}/status", body_status => $body);
+  }
+  return $result->{result};
+}
+
+
+sub read_session {
+  my $self = shift;
+
+  my $file;
+  open($file, "<", "cache/session_id") or return;
+  $self->{session_id} = <$file>;
+  $self->{session_time} = (stat $file)[9];
+  close($file);
+  chomp($self->{session_id});
+}
+
+sub write_session {
+  my $self = shift;
+
+  my $file;
+  open($file, ">", "cache/session_id") or die "Couldn't write cache/session_id: $!";
+  print $file "$self->{session_id}\n";
+  close($file);
+}
+
+sub session_id {
+  my $self = shift;
+
+# warn "Known session: $self->{session_id}\n";
+  return $self->{session_id} if $self->{session_time} >= time() - 3600 * 1.5;
+  $self->read_session();
+# warn "Preexisting session: $self->{session_id}\n";
+  return $self->{session_id} if $self->{session_time} >= time() - 3600 * 1.5;
+  my $result = $self->call(empire => login => $self->{empire_name}, $self->{empire_password}, $self->{api_key});
+# warn "Created session: $self->{session_id}\n";
+  return $self->{session_id} if $self->{session_time} >= time() - 3600 * 1.5;
+  croak "Couldn't get session_id";
+}
+
+sub empire_status {
+  my $self = shift;
+
+  my $result = $self->read_json("cache/empire/$self->{empire_name}/status");
+  return $result if $result->{_time} >= time() - 500;
+  my $result = $self->call(empire => login => $self->{empire_name}, $self->{empire_password}, $self->{api_key});
+  return $result->{status}{empire} if $result->{status}{empire};
+  croak "Couldn't get empire status";
+}
+
+sub body_status {
+  my $self = shift;
+  my $body_id = shift;
+
+  my $result = $self->read_json("cache/body/$body_id/status");
+  return $result if $result->{_time} >= time() - 500;
+  my $result = $self->body_buildings($body_id);
+  return $result->{status}{body} if $result->{status}{body};
+  croak "Couldn't get body status";
+}
+
+sub body_buildings {
+  my $self = shift;
+  my $body_id = shift;
+
+  my $result = $self->read_json("cache/body/$body_id/buildings");
+  return $result if $result->{_invalid} > time();
+  my $result = $self->call(body => get_buildings => $body_id);
+  my @completions;
+  for my $building (values(%{$result->{buildings}})) {
+    push(@completions, parse_time($building->{pending_build}{end})) if $building->{pending_build};
+    push(@completions, parse_time($building->{work         }{end})) if $building->{work};
+  }
+  $result->{_invalid} = List::Util::min(time() + 3600, @completions);
+  $self->write_json("cache/body/$body_id/buildings", buildings => $result);
+  return $result;
+}
+
+sub body_buildable {
+  my $self = shift;
+  my $body_id = shift;
+
+  my $result = $self->read_json("cache/body/$body_id/buildable");
+  return $result if $result->{_invalid} > time();
+  my $result = $self->call(body => get_buildable => $body_id);
+  my $buildings = $self->body_buildings($body_id);
+  my @completions;
+  for my $building (values(%{$buildings->{buildings}})) {
+    next unless $building->{pending_build};
+    next unless $building->{name} =~ /Oversight|Ore Refinery|Intelligence|University/;
+    push(@completions, parse_time($building->{pending_build}{end}));
+  }
+  $result->{_invalid} = List::Util::min(time() + 600, @completions);
+  $self->write_json("cache/body/$body_id/buildable", buildable => $result);
+  return $result;
+}
+
+sub body_build {
+  my $self = shift;
+  my $body_id = shift;
+  my $building_name = shift;
+
+  my $url = "";
+  my %plots;
+  my $existing = $self->body_buildings($body_id);
+  for my $building (values %{$existing->{buildings}}) {
+    $plots{$building->{x},$building->{y}} = 1;
+    $url = $building->{url} if $building->{name} eq $building_name;
+  }
+  my @plots;
+  for my $x (-5..5) {
+    for my $y (-5..5) {
+      push(@plots, [ $x, $y ]) unless $plots{$x,$y};
+    }
+  }
+  my $place = $plots[int(rand(@plots))];
+
+  $url ||= $self->body_buildable($body_id)->{buildable}{$building_name}{url};
+
+  return $self->building_build($url, $body_id, @$place);
+}
+
+sub building_build {
+  my $self = shift;
+  my $url = shift;
+  my $body_id = shift;
+  my $x = shift;
+  my $y = shift;
+
+  my $result = $self->call($url => build => $body_id, $x, $y);
+  unlink("cache/body/$body_id/buildings") if $result;
+  unlink("cache/body/$body_id/buildable") if $result && $url =~ /oversight|orerefinery|intelligence|university/;
+  return $result;
+}
+
+sub building_upgrade {
+  my $self = shift;
+  my $url = shift;
+  my $building_id = shift;
+
+  my $result = $self->call($url => upgrade => $building_id);
+  unlink("cache/body/$result->{status}{body}{id}/buildings") if $result;
+  unlink("cache/body/$result->{status}{body}{id}/buildable") if $result && $url =~ /oversight|orerefinery/;
+  unlink("cache/building/$building_id/view") if $result;
+  return $result;
+}
+
+sub building_view {
+  my $self = shift;
+  my $url = shift;
+  my $building_id = shift;
+
+  my $result = $self->read_json("cache/building/$building_id/view");
+  return $result if $result->{_invalid} > time();
+  my $result = $self->call($url, view => $building_id);
+  my @completions;
+  for my $building ($result->{building}) {
+    push(@completions, parse_time($building->{pending_build}{end})) if $building->{pending_build};
+    push(@completions, parse_time($building->{work         }{end})) if $building->{work};
+  }
+  $result->{_invalid} = List::Util::min(time() + 3600, @completions);
+  $self->write_json("cache/building/$building_id/view", building_view => $result);
+  return $result;
+}
+
+sub building_stats_for_level {
+  my $self = shift;
+  my $url = shift;
+  my $building_id = shift;
+  my $level = shift;
+
+  my $result = $self->read_json("cache/building/$building_id/stats_$level");
+  return $result if $result;
+  my $result = $self->call($url, get_stats_for_level => $building_id, $level);
+  $self->write_json("cache/building/$building_id/stats_$level", building_stats => $result);
+  return $result;
+}
+
+1;
---
+++ project.pl
@@ -0,0 +1,157 @@
+#!/usr/bin/perl
+
+use strict;
+
+use Getopt::Long;
+use JSON::XS;
+use List::Util;
+use Time::Local;
+
+my $config_name = "config.json";
+my $body_name;
+my $initial_name;
+
+GetOptions(
+  "config=s"        => \$config_name,
+  "body=s"          => \$body_name,
+  "initial|start=s" => \$initial_name,
+) or die "$0 --config=foo.yaml --body=Bar\n";
+
+my $options = "";
+$options .= " --config $config_name" if $config_name;
+$options .= " --body $body_name" if $body_name;
+
+my $info = decode_json(`get_buildable_info $options`);
+
+my $initial;
+if ($initial_name) {
+  $initial = decode_json(`cat $initial_name`);
+} else {
+  $initial = decode_json(`get_planetary_state $options`);
+}
+
+my @buildings = @{$initial->{buildings}};
+my %stocks    = %{$initial->{stored}};
+my $time      =   $initial->{time};
+
+my %production;
+my %capacity;
+
+sub format_time {
+  my $time = shift;
+
+  my @elems = reverse((localtime($time))[0..5]);
+  $elems[0] += 1900;
+  $elems[1]++;
+  sprintf("%4d-%02d-%02d %02d:%02d:%02d", @elems);
+}
+
+sub emit {
+  my $message = shift;
+  print format_time($time)." $message\n";
+}
+
+sub compute_production {
+  %production = ();
+  %capacity   = ();
+  for my $building (@buildings) {
+    for my $resource (qw(food ore water energy waste)) {
+      $production{$resource} += $info->{$building->{name}}{$building->{level}}{production}{$resource};
+      $capacity  {$resource} += $info->{$building->{name}}{$building->{level}}{capacity  }{$resource};
+    }
+  }
+  emit("production: ".join("; ", map { "$_ $production{$_}" } qw(food ore water energy waste)));
+  for my $resource (qw(food ore water energy)) {
+    die "Unsustainable! Production of $resource is less than 1!\n" if $production{$resource} < 1;
+  }
+  die "Unsustainable! Stock of waste is less than 1!\n" if $production{waste} < 1 && $stocks{waste} < 1;
+}
+
+sub time_until {
+  my $amount = shift;
+  my $act = shift;
+
+  my $wait = 0;
+  my $on;
+  for my $resource (qw(food ore water energy)) {
+    my $target = $amount->{$resource} + $production{$resource} / 10;
+    die "Cannot $act: Insufficient capacity for $resource: need $amount->{$resource}, capacity $capacity{$resource}\n"
+      if $target > $capacity{$resource};
+    
+    if ($target > $stocks{$resource}) {
+      my $delay = int(($target - $stocks{$resource}) / $production{$resource} * 3600 + 1);
+      if ($wait < $delay) {
+        $wait = $delay;
+        $on = int($target - $stocks{$resource}) . " $resource";
+      }
+    }
+  }
+  emit("waiting $wait seconds for $on") if $on;
+  return $wait;
+}
+
+sub subtract {
+  my $amount = shift;
+  for my $resource (qw(food ore water energy)) {
+    $stocks{$resource} -= $amount->{$resource};
+  }
+  $stocks{waste} += $amount->{waste};
+}
+
+sub project {
+  my $delta = shift;
+
+  for my $resource (qw(food ore water energy waste)) {
+    $stocks{$resource} += $production{$resource} * $delta / 3600;
+    $stocks{$resource} = 0 if $stocks{$resource} < 0;
+    $stocks{$resource} = $capacity{$resource} if $stocks{$resource} > $capacity{$resource};
+  }
+  $time += $delta;
+  emit(join("; ", map { sprintf("%s %d/%d", $_, $stocks{$_}, $capacity{$_}) } qw(food ore water energy waste)));
+}
+
+compute_production();
+my $line;
+while ($line = <>) {
+  if ($line =~ /^build (.*)/) {
+    my $name = $1;
+
+    project(time_until($info->{$name}{1}{cost}, "build $name"));
+    emit("Starting build $name");
+    subtract($info->{$name}{1}{cost});
+    project($info->{$name}{1}{cost}{time});
+    emit("Finished build $name");
+
+    push(@buildings, { name => $name, level => 1 });
+    compute_production();
+  }
+  if ($line =~ /^upgrade (\d+) (.*)/) {
+    my $level = $1;
+    my $name = $2;
+
+    my $index;
+    while ($index < @buildings && ($buildings[$index]{name} ne $name || $buildings[$index]{level} != $level)) {
+      $index++;
+    }
+    die "No $level $name to upgrade\n" unless $index < @buildings;
+
+    project(time_until($info->{$name}{$level}{upgrade}, "upgrade $level $name"));
+    emit("Starting upgrade $level $name");
+    subtract($info->{$name}{$level}{upgrade});
+    project($info->{$name}{$level}{upgrade}{time});
+    emit("Finished upgrade $level $name");
+    
+    $buildings[$index]{level}++;
+    compute_production();
+  }
+  if ($line =~ /^recycle (\d+) (.*)/) {
+    my $amount = $1;
+    my $resource = $2;
+
+    emit("Starting recycle $amount $resource");
+    subtract({ waste => -$amount });
+    project($amount / 1578 * 3600);
+    emit("Finished recycle $amount $resource");
+    subtract({ $resource => -$amount });
+  }
+}
