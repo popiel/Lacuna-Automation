9b94970b5234e82efd826855f1e214e066619e10
Ivan Heffner <iheffner@marchex.com>
2011-02-14 18:14:04 -0800

    added usage(), debug(), verbose(), output() to Tools.pm
    expects %main::opts

--- Tools.pm
+++ Tools.pm
@@ -13,6 +13,15 @@ use base 'Exporter';
 
 our @EXPORT_OK = qw(
     normalize_planet
+    usage
+    debug
+    verbose
+    output
+);
+
+our %EXPORT_TAGS = (
+    'all' => \@EXPORT_OK,
+    'messages' => [qw(usage debug verbose output)],
 );
 
 our $VERSION = '0.01';
@@ -23,6 +32,25 @@ sub normalize_planet {
     $planet_name =~ s/\W//g;
     $planet_name = lc($planet_name);
     return $planet_name;
+}
+
+sub usage {
+    die $main::usage;
+}
+
+sub debug {
+    return unless ( grep { $main::opts{$_} } qw( debug d ) );
+    output(' ==== ',@_);
+}
+
+sub verbose {
+    return unless ( grep { $main::opts{$_} } qw( verbose v debug ) );
+    output(' -- ',@_);
+}
+
+sub output {
+    return if ( grep { $main::opts{$_} } qw( quiet q ) );
+    print scalar(localtime),': ',@_,"\n";
 }
 
 1;
