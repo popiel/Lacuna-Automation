35375a5629de71f42a0669c803e162ec452b441a
ysth <sthoenna@gmail.com>
2012-04-17 12:18:59 -0700

    get_buildings: body selector, show body id, add option to show all of selected types

--- get_buildings.pl
+++ get_buildings.pl
@@ -9,21 +9,34 @@ use JSON::PP;
 my $config_name = "config.json";
 my $body_name;
 my $name = "Trade|Subspace|Mission|Port";
+my $every = "Warehouse";
 
 GetOptions(
   "config=s" => \$config_name,
   "name=s"   => \$name,
-) or die "$0 --config=foo.json --body=Bar\n";
+  "body=s"   => \$body_name,
+  "every=s"  => \$every,
+) or die "$0 --config=foo.json --name=Bar\n";
 
 my $client = Client->new(config => $config_name);
 my $planets = $client->empire_status->{planets};
 
 my %plans;
 for my $body_id (sort { $planets->{$a} cmp $planets->{$b} } keys(%$planets)) {
-  print "$planets->{$body_id}:\n";
+  next if $body_name && $body_name ne $planets->{$body_id};
+  print "$planets->{$body_id} ($body_id):\n";
   my $buildings = $client->body_buildings($body_id);
   my @buildings = map { { %{$buildings->{buildings}{$_}}, id => $_ } } keys(%{$buildings->{buildings}});
-  my %buildings = map { $_->{name}, $_->{id} } @buildings;
-  my %levels    = map { $_->{name}, $_->{level} } sort { $a->{level} <=> $b->{level} } @buildings;
-  printf("  %-22slevel %2d, id %d\n", "$_:", $levels{$_}, $buildings{$_}) for (sort grep { /$name/ } keys %buildings);
+  my %buildings;
+  my %levels;
+  for my $building (@buildings) {
+    my $key = $building->{name};
+    $key .= " $building->{id}" if $key =~ $every;
+    $buildings{$key} = $building->{id};
+    $levels{$key} = $building->{level};
+  }
+  for my $key (sort grep { /$name/ } keys %buildings) {
+    (my $name = $key) =~ s/ \Q$buildings{$key}\E\z//;
+    printf("  %-28slevel %2d, id %d\n", "$name:", $levels{$key}, $buildings{$key});
+  }
 }
