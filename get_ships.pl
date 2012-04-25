00706171046080eb862a51fce0d0097a5706d943
ysth <sthoenna@gmail.com>
2012-04-25 11:09:52 -0700

    get_ships: add --body, get_buildings: make --body case insensitive

--- get_ships.pl
+++ get_ships.pl
@@ -1,23 +1,26 @@
 #!/usr/bin/perl
 
 use strict;
+use warnings;
 
 use Client;
 use Getopt::Long;
 use JSON::XS;
 
 my $config_name = "config.json";
-my $body_name;
+my $body_name = "";
 
 GetOptions(
   "config=s" => \$config_name,
+  "body=s" => \$body_name,
 ) or die "$0 --config=foo.json --body=Bar\n";
 
 my $client = Client->new(config => $config_name);
 my $planets = $client->empire_status->{planets};
 
 for my $body_id (sort { $planets->{$a} cmp $planets->{$b} } keys(%$planets)) {
-  print "$planets->{$body_id}:\n";
+  next if $body_name && $planets->{$body_id} !~ /\Q$body_name/i;
+  print "$planets->{$body_id} ($body_id):\n";
   my $buildings = $client->body_buildings($body_id);
   my @buildings = map { { %{$buildings->{buildings}{$_}}, id => $_ } } keys(%{$buildings->{buildings}});
   my $trade = (grep($_->{name} eq "Trade Ministry", @buildings))[0];
