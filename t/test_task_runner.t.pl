7647d73ade4c519ffc1231f053b28a3c31f16bdd
Xaositect <xaositect@lanndark.com>
2012-04-28 22:16:46 -0700

    Test callback override of next scheduled time

--- t/test_task_runner.t.pl
+++ t/test_task_runner.t.pl
@@ -34,6 +34,7 @@ isa_ok($runner, 'Client::TaskRunner');
 				my ($self,$runner) = @_; 
 				$self->stop_repeating() if (++$ran_stub > 2); 
 			},
+			'description' => 'Basic one-off',
 		);
 	};
 	isa_ok($task, 'Client::Task');
@@ -44,6 +45,7 @@ isa_ok($runner, 'Client::TaskRunner');
 	is($ran_stub, 1);
 
 	$task->repeat_after(1);
+	$task->description('Basic repeating');
 	$runner->add_task($task);
 	$runner->run();
 	is($ran_stub, 3);
@@ -57,28 +59,49 @@ isa_ok($runner, 'Client::TaskRunner');
 	$runner->add_task(Client::Task->new( 'callback' => sub { sleep 3; push(@ran_stub, 'cb1'); } ));
 	$runner->add_task(Client::Task->new( 
 		'callback' => sub { push(@ran_stub, 'cb2') }, 
-		'next_run' => time() + 2
+		'next_run' => time() + 2,
+		'description' => 'Longer task than next scheduled',
 	));
 	$runner->run();
 	is_deeply(\@ran_stub, ['cb1', 'cb2']);
 }
-
 {
+	# verify that the callback can override the next run time
+	my $ran_stub = 0;
+	$runner = Client::TaskRunner->new('client' => $client, 'debug' => 1);
+	$runner->add_task(Client::Task->new( 
+		repeat_after => 30, 
+		callback => sub { 
+			if (++$ran_stub > 1) { 
+				$_[0]->stop_repeating();
+			} else { 
+				$_[0]->next_run(time()); 
+			}
+		},
+		description => 'Callback override of schedule',
+	));
+	my $start = time();
+	$runner->run();
+	ok(time() - $start < 30);
+}
+{
+	# verify cron scheduling support
 	my $ran_stub = 0;
 	$runner = Client::TaskRunner->new('client' => $client, 'debug' => 1);
 	# TODO test cron scheduling
 	$runner->add_task(Client::Task->new(
-		# TODO, replace next_run default with cron next run
 		'cron_spec' => DateTime::Event::Cron->new('* * * * *'),
 		'callback'  => sub { 
 			$_[0]->remove_cron() if ++$ran_stub > 1; 
 		},
-		'description' => 'Testing cron scheduling',
+		'description' => 'Cron style scheduling',
 	));
 	$runner->run();
 	is($ran_stub, 2);
-
 }
 
-done_testing(10);
+
+
+
+done_testing(11);
 
