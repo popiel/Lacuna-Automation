0bf464e652ca600c7187b9709a31710a5038eb1d
T. Alexander Popiel <popiel@wolfskeep.com>
2012-05-13 07:56:39 -0700

    Allow digger to use embassy to get useful ores

--- digger.pl
+++ digger.pl
@@ -74,17 +74,41 @@ for my $recipe (@recipes) {
     $bias{$ore} = $glyphs{$ore} - $average;
   }
   emit(sprintf("%5d %-12s %5d %-12s %5d %-12s %5d %-12s", map { $glyphs{$_}, $_ } @$recipe));
+  emit(sprintf("  %5d %-12s %5d %-12s %5d %-12s %5d %-12s", map { $bias{$_}, "bias" } @$recipe));
 }
 
 for my $body_id (@body_ids) {
   next if Client::parse_time($arches{$body_id}{work}{end}) > time();
   my $ores = $client->ores_for_search($arches{$body_id}{id});
   my @ores = sort { $bias{$a} <=> $bias{$b} } keys (%{$ores->{ore}});
+  @ores = grep { $bias{$_} < 0 } @ores;
   if (@ores) {
-    emit("Searching for $ores[0] glyph", $body_id);
+    emit("Searching for $ores[0] glyph (bias $bias{$ores[0]})", $body_id);
     $noaction or $client->archaeology_search($arches{$body_id}{id}, $ores[0]);
     $bias{$ores[0]}++;
+    next;
   }
+  my $embassy = eval { $client->find_building($body_id, "Embassy"); };
+  next unless $embassy;
+
+  my $result = $client->call(embassy => view_stash => $embassy->{id});
+  next unless $result->{exchanges_remaining_today} && $result->{max_exchange_size} >= 10000;
+  my %stored = %{$result->{stored}};
+  my %stash  = %{$result->{stash}};
+
+  @ores = sort { $bias{$a} <=> $bias{$b} } keys %bias;
+  @ores = grep { $stash{$_} >= 10000 && $bias{$_} < 0 } @ores;
+  next unless @ores;
+
+  my %wanted = ( $ores[0] => 10000 );
+  my %extra = map { $_, $stored{$_} } keys %bias;
+  delete $extra{$ores[0]};
+  my %giving = $client->select_exchange(\%stash, \%extra, \%wanted);
+  emit("Exchanging ". join(", ", map { "$giving{$_} $_" } keys(%giving)). " for ". join(", ", map { "$wanted{$_} $_" } keys(%wanted)));
+  $noaction or eval { $client->call(embassy => exchange_with_stash => $embassy->{id}, { %giving }, { %wanted }); };
+  emit("Searching for $ores[0] glyph (bias $bias{$ores[0]})", $body_id);
+  $noaction or eval { $client->archaeology_search($arches{$body_id}{id}, $ores[0]); };
+  $bias{$ores[0]}++;
 }
 
 sub emit {
