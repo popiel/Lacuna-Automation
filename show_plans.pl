6abfee08da974f56d5d43fe31750c4d817b45cdc
T. Alexander Popiel <popiel@wolfskeep.com>
2012-07-01 08:57:45 -0700

    Merging find_plans back into show_plans

--- show_plans.pl
+++ show_plans.pl
@@ -4,34 +4,44 @@ use strict;
 
 use Client;
 use Getopt::Long;
+use List::Util qw(sum);
 use JSON::PP;
 
 my $config_name = "config.json";
 my @body_name;
+my $total_only;
 
 GetOptions(
   "config=s" => \$config_name,
   "body=s"   => \@body_name,
-) or die "$0 --config=foo.json --body=Bar\n";
+  "total!"   => \$total_only,
+) or die "$0 --config=foo.json --body=Bar --total_only\n";
 
 my $client = Client->new(config => $config_name);
 my $planets = $client->empire_status->{planets};
 
 my %plans;
 for my $body_id (keys(%$planets)) {
-  next unless !@body_name || grep { $planets->{$body_id} =~ /$_/ } @body_name;
-  my $buildings = $client->body_buildings($body_id);
-  my @buildings = map { { %{$buildings->{buildings}{$_}}, id => $_ } } keys(%{$buildings->{buildings}});
-  my $trade = (grep($_->{name} eq "Trade Ministry", @buildings))[0];
-  next unless $trade;
+  my $body_name = $planets->{$body_id};
+  next unless !@body_name || grep { $body_name =~ /$_/ } @body_name;
 
-  my $plans = $client->call(trade => get_plans => $trade->{id});
-  printf("Got %d plans from %s\n", scalar(@{$plans->{plans}}), $planets->{$body_id});
+  my $plans = eval { $client->body_plans($body_id) };
+  next unless $plans;
+
+  printf("Got %d plans from %s\n", sum(map { $_->{quantity} } @{$plans->{plans}}), $body_name);
   for my $plan (@{$plans->{plans}}) {
-    $plans{"$plan->{name} ($plan->{level}+$plan->{extra_build_level})"} ||= [];
-    push(@{$plans{"$plan->{name} ($plan->{level}+$plan->{extra_build_level})"}}, $plan->{id});
+    $plans{"$plan->{name} ($plan->{level}+$plan->{extra_build_level})"} ||= {};
+    $plans{"$plan->{name} ($plan->{level}+$plan->{extra_build_level})"}{$body_name} = $plan->{quantity};
   }
 }
+print "----\n";
 for my $name (sort keys %plans) {
-  printf("%5d %-30s %s\n", scalar(@{$plans{$name}}), $name, join(", ", @{$plans{$name}}[0..3]));
+  my $plans = $plans{$name};
+  my $total = sum(values %$plans);
+  printf("%5d %-30s\n", $total, $name);
+  unless ($total_only) {
+    for my $body (sort keys %$plans) {
+      printf("  %5d %s\n", $plans->{$body}, $body);
+    }
+  }
 }
