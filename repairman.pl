d6834dce82def33ba0e546b199132f8894a00d71
Ivan Heffner <iheffner@gmail.com>
2013-01-23 03:10:55 -0800

    put guts of repairman into sub repair()

--- repairman.pl
+++ repairman.pl
@@ -34,54 +34,60 @@ if ($body_name) {
   }
   exit(1) unless $body_id || !$quiet_no_body;
   die "No matching planet for name $body_name\n" unless $body_id;
+  repair($body_id);
 } else {
   $body_id = $client->empire_status->{home_planet_id};
+  repair($body_id);
 }
 
-my $buildings = $client->body_buildings($body_id);
-$body_name = $client->body_status($body_id)->{name};
+sub repair {
+  my $body_id = shift;
 
-my @buildings = map { { %{$buildings->{buildings}{$_}}, id => $_ } } keys(%{$buildings->{buildings}});
+  my $buildings = $client->body_buildings($body_id);
+  $body_name = $client->body_status($body_id)->{name};
 
-my @cost;
-for my $building (@buildings) {
-  next if $building->{efficiency} == 100;
-  my $view = $client->building_view($building->{url}, $building->{id})->{building};
-  my $cost = List::Util::sum( values %{ $view->{repair_costs} } );
-  if ( $cost == 0 ) {
-    $client->building_repair($building->{url}, $building->{id});
-    emit("Repaired $building->{name} from $building->{efficiency}%");
-  }
-  else {
-    push @cost, [ $building, $cost ];
-  }
-}
+  my @buildings = map { { %{$buildings->{buildings}{$_}}, id => $_ } } keys(%{$buildings->{buildings}});
 
-my @sorted = map $_->[0], sort { $a->[1] <=> $b->[1] } @cost;
-
-while (@sorted) {
-  # By default, repair the cheapest first
-  my $pick = $sorted[0];
-
-  # Repair platforms first if we're at negative plots
-  if ($client->body_status($body_id)->{plots_available} < 0) {
-    my @platforms_first = (
-      grep { $_->{url} =~ /platform/ } @sorted,
-      grep { $_->{url} !~ /platform/ } @sorted,
-    );
-    $pick = $platforms_first[0];
+  my @cost;
+  for my $building (@buildings) {
+    next if $building->{efficiency} == 100;
+    my $view = $client->building_view($building->{url}, $building->{id})->{building};
+    my $cost = List::Util::sum( values %{ $view->{repair_costs} } );
+    if ( $cost == 0 ) {
+      $client->building_repair($building->{url}, $building->{id});
+      emit("Repaired $building->{name} from $building->{efficiency}%");
+    }
+    else {
+      push @cost, [ $building, $cost ];
+    }
   }
 
-  # If we're repairing a Tyleon, repair the most expensive one
-  if ($pick->{name} =~ /Tyleon/) {
-    my @tyleons = grep { $_->{name} =~ /Tyleon/ } @sorted;
-    $pick = $tyleons[$#tyleons];
+  my @sorted = map $_->[0], sort { $a->[1] <=> $b->[1] } @cost;
+
+  while (@sorted) {
+    # By default, repair the cheapest first
+    my $pick = $sorted[0];
+
+    # Repair platforms first if we're at negative plots
+    if ($client->body_status($body_id)->{plots_available} < 0) {
+      my @platforms_first = (
+        grep { $_->{url} =~ /platform/ } @sorted,
+        grep { $_->{url} !~ /platform/ } @sorted,
+      );
+      $pick = $platforms_first[0];
+    }
+
+    # If we're repairing a Tyleon, repair the most expensive one
+    if ($pick->{name} =~ /Tyleon/) {
+      my @tyleons = grep { $_->{name} =~ /Tyleon/ } @sorted;
+      $pick = $tyleons[$#tyleons];
+    }
+
+    @sorted = grep { $_ ne $pick } @sorted;
+
+    $client->building_repair($pick->{url}, $pick->{id});
+    emit("Repaired $pick->{name} from $pick->{efficiency}%");
   }
-
-  @sorted = grep { $_ ne $pick } @sorted;
-
-  $client->building_repair($pick->{url}, $pick->{id});
-  emit("Repaired $pick->{name} from $pick->{efficiency}%");
 }
 
 sub emit {
